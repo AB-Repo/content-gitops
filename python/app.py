331641de40813f33c7b814f9546399f8a55497d4
AB-Repo <82955140+AB-Repo@users.noreply.github.com>
2023-11-04 22:03:48 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello LA Student AGAN AGAN AGAN"
+    return "Hello LA Student AGAN AGAN AGAN AGAN"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
